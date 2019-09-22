e3dd105d7ac55aa3edfd4cb5a5f4a98ce4c01ac0
Sanket Kotak <sanket.903@gmail.com>
2019-09-23 08:40:57 +1000

    Remove Commented Code

--- PayslipGenerator.Model/PaymentPeriod.cs
+++ PayslipGenerator.Model/PaymentPeriod.cs
@@ -6,44 +6,7 @@ namespace PayslipGenerator.Model
 {
     public sealed class PaymentPeriod
     {
-        /*
-        public DateTime StartDate { get; internal set; }
-        public DateTime EndDate { get; internal set; }
-
-        internal PaymentPeriod() { }
-        public PaymentPeriod(DateTime startDateTime, DateTime endDateTime)
-        {
-            StartDate = startDateTime;
-            EndDate = endDateTime;
-        }
-
-        public static bool TryParseFromDateRangeString(string dateRange, out PaymentPeriod paymentPeriod)
-        {
-            DateTime _startDateTime = DateTime.Now;
-            DateTime _endDateTime = DateTime.Now;
-
-            string[] paymentDateTime = dateRange.Split(new[] { '-' }, StringSplitOptions.RemoveEmptyEntries);
-            if (paymentDateTime[0] != null)
-            {
-                DateTime.TryParse(paymentDateTime[0], out _startDateTime);
-
-                if (paymentDateTime[1] != null)
-                {
-                    DateTime.TryParse(paymentDateTime[1], out _endDateTime);
-                    paymentPeriod = new PaymentPeriod(_startDateTime, _endDateTime);
-                    return true;
-                }
-            }
-            paymentPeriod = default(PaymentPeriod);
-            return false;
-        }
-
-        public override string ToString()
-        {
-            return $"{StartDate.ToShortDateString()} - {EndDate.ToShortDateString()}";
-        }*/
-
-
+      
         public DateTime StartDate { get; internal set; }
         public DateTime EndDate { get; internal set; }
 
